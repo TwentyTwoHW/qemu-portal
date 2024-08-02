baea7c860d68847d2925ceb1243970b4eaabfd6c
Alekos Filini <alekos.filini@gmail.com>
2024-08-03 00:19:43 +0200

    add shell.nix

---
+++ shell.nix
@@ -0,0 +1,5 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    # nativeBuildInputs is usually what you want -- tools you need to run
+    nativeBuildInputs = with pkgs; [ meson ninja python3 pkg-config glib ];
+}
